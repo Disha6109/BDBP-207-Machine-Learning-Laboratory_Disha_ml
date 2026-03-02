ffec87b8506f8823396967eba9a13a3add274c87
Disha6109 <disha2001nayak@gmail.com>
2026-03-02 15:29:57 +0530

    Add files via upload

---
+++ ML_LAB/LAB5/question1.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+X = np.array([[10], [11], [12], [13], [14]])
+
+y = np.array([3, 6 ,9 ,12 ,15 ])
+
+X = np.c_[np.ones(X.shape[0]), X]
+
+theta = np.zeros(X.shape[1])
+
+learning_rate = 0.01
+
+batch_set = 2
+
+
+m = len(y)
+
+no_of_steps= 1000
+
+
+for i in range(no_of_steps):
+
+    numbers = np.random.permutation(m)
+
+    Xb = X[numbers[:batch_set]]
+
+    yb = y[numbers[:batch_set]]
+
+    predictions = Xb @ theta
+
+    error = predictions - yb
+
+
+    gradient = (Xb.T @ error) / batch_set
+
+    theta = theta - learning_rate * gradient
+
+
+print("Final value of Theta:", theta)
+
---
+++ ML_LAB/LAB5/question2.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+def sigmoid(z):
+
+    return 1/(1+np.exp(-z))
+
+
+z = np.linspace(-10,10,100)
+
+plt.plot(z,sigmoid(z))
+
+plt.xlabel("z")
+
+plt.ylabel("Sigmoid_function(z)")
+
+plt.title("Sigmoid Function")
+
+plt.grid()
+
+plt.show()
